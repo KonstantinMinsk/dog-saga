0b3df214493fb645cb9fc6796be421940af56bf8
KonstantinMinsk <Sokolov.k007@yandex.ru>
2020-12-01 00:26:35 +0300

    feat: add sagas.js, redux.js

---
+++ README.md
@@ -0,0 +1,1 @@
+https://hackernoon.com/redux-saga-tutorial-for-beginners-and-dog-lovers-aa69a17db645
---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+import { connect } from "react-redux";
+
+const App = (props) => {
+    const { fetching, dog, onRequestDog, error } = props;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={dog || logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Dog Saga</h1>
+          {dog ? (
+            <p className="App-intro">Keep clicking for new dogs</p>
+          ) : (
+            <p className="App-intro">Replace the React icon with a dog!</p>
+          )}
+
+          {fetching ? (
+            <button disabled>Fetching...</button>
+          ) : (
+            <button onClick={onRequestDog}>Request a Dog</button>
+          )}
+
+          {error && <p style={{ color: "red" }}>Uh oh - something went wrong!</p>}
+        </header>
+      </div>
+    );
+}
+
+const mapStateToProps = state => {
+  return {
+    fetching: state.fetching,
+    dog: state.dog,
+    error: state.error
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onRequestDog: () => dispatch({ type: "API_CALL_REQUEST" })
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
